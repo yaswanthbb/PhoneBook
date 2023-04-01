00829704e44557887a0660009e7cd6a21b76472f
Destro001 <byaswanth2003@gmail.com>
2023-04-01 11:58:09 +0530

    Creating building

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
